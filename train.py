dd92355cf935bda3af2a7a97e7e26a7347ed6dde
isak-sjoholm <isak.sjholm@gmail.com>
2026-09-08 23:00:39 +0200

    edit train script

--- train.py
+++ train.py
@@ -6,6 +6,8 @@
 import pandas as pd
 import glob
 import os
+import json
+
 
 ## Google
 import gspread
@@ -21,8 +23,8 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
-# Set up Spotify integration
 
+# Set up Spotify integration
 CLIENT_ID = os.environ.get('SPOTIFY_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('SPOTIFY_CLIENT_SECRET')
 
@@ -42,6 +44,51 @@ token_info = sp_oauth.refresh_access_token(REFRESH_TOKEN)
 sp = spotipy.Spotify(auth=token_info['access_token'])
 
 user = sp.current_user()
-print(f"Inloggad som: {user['display_name']} ({user['id']})")
+print(f"Logged in as: {user['display_name']} ({user['id']})")
 
 playlist_id = "0kmA3oRKWHZNrqS2zByAR3"
+
+
+
+# Read & deduplicate Spotify data
+path = 'data/'
+files = glob.glob(path + '*.csv')
+
+dfs = []
+
+for file in files:
+    try:
+        df = pd.read_csv(file)
+        df['källfil'] = file.split('/')[-1]
+        dfs.append(df)
+
+    except Exception as e:
+        print(f"Could not read {file}: {e}")
+
+combined = pd.concat(dfs, ignore_index=True)
+print(f"Total numer of rows (inc. duplicates): {len(combined)}")
+
+deduped = combined.drop_duplicates(subset='Låtens URI', keep='first')
+print(f"Total number of unique songs: {len(deduped)}")
+
+
+
+# Read song feedback from Google Sheets & join with audio Spotify data
+
+## Authenticate to Google Sheets via service account
+service_account_info = json.loads(os.environ.get('GOOGLE_SERVICE_ACCOUNT_JSON'))
+scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+gc = gspread.authorize(creds)
+
+## Open & read Google Sheets with song feedbacks
+sheet = gc.open("Run Music Feedback").sheet1
+rows = sheet.get_all_values()
+
+## Save feedback in df
+feedback_df = pd.DataFrame(rows, columns=['created_at', 'track_uri', 'track_position', 'feedback'])
+feedback_df['track_position'] = feedback_df['track_position'].astype(int)
+
+## Join with Spotify data
+training_data = feedback_df.merge(deduped, left_on='track_uri', right_on='Låtens URI', how='left')
+training_data_clean = training_data[training_data['Låtens URI'].notna()].copy()
